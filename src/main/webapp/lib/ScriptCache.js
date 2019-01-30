edcf5fc0db133c3798ad66e0d46d48cc352e27c8
wdrussell <wdrussell2015@gmail.com>
2019-01-29 21:02:01 -0500

    Added Google map api frontend loader

--- src/main/webapp/lib/ScriptCache.js
+++ src/main/webapp/lib/ScriptCache.js
@@ -1,92 +1,72 @@
 let counter = 0;
-let scriptMap = typeof window !== 'undefined' && window._scriptMap || new Map();
-const window = require('./windowOrGlobal');
+let scriptMap = new Map();
 
 export const ScriptCache = (function(global) {
-    global._scriptMap = global._scriptMap ||scriptMap;
-    return function ScriptCache(scripts) {
+    return function ScriptCache (scripts) {
         const Cache = {};
 
-        Cache._onLoad = function(key) {
+        Cache._onLoad = function (key) {
             return (cb) => {
-                let registered = true;
-
-                function unregister() {
-                    registered = false;
-                }
-
                 let stored = scriptMap.get(key);
-
                 if (stored) {
                     stored.promise.then(() => {
-                        if (registered) {
-                            stored.error ? cb(stored.error) : cb(null, stored)
-                        }
-
-                        return stored;
-                    });
+                        stored.error ? cb(stored.error) : cb(null, stored)
+                    })
                 }
-                return unregister;
             }
-        }
+        };
 
         Cache._scriptTag = (key, src) => {
             if (!scriptMap.has(key)) {
-                //If environment doesnt have access to 'document' just return null
-                if (typeof document === 'undefined') {
-                    return null;
-                }
-
                 let tag = document.createElement('script');
                 let promise = new Promise((resolve, reject) => {
-                    let resolved = false;
-                    let errored = false;
-                    let body = document.getElementsByTagName('body')[0];
+                    let resolved = false,
+                        errored = false,
+                        body = document.getElementsByTagName('body')[0];
 
                     tag.type = 'text/javascript';
-                    tag.async = false; //Load tags in order
+                    tag.async = false; // Load in order
 
-                    const cbName = `loaderCB${counter++}${Date.now()}`;//callbackName
+                    const cbName = `loaderCB${counter++}${Date.now()}`;
                     let cb;
 
                     let handleResult = (state) => {
-                        return (event) => {
+                        return (evt) => {
                             let stored = scriptMap.get(key);
                             if (state === 'loaded') {
                                 stored.resolved = true;
                                 resolve(src);
-                            }
-                            else if (state === 'error') {
+                                // stored.handlers.forEach(h => h.call(null, stored))
+                                // stored.handlers = []
+                            } else if (state === 'error') {
                                 stored.errored = true;
-                                reject(event);
+                                // stored.handlers.forEach(h => h.call(null, stored))
+                                // stored.handlers = [];
+                                reject(evt)
                             }
-                            stored.loaded = true;
 
                             cleanup();
                         }
-                    }
+                    };
 
                     const cleanup = () => {
-                        if(global[cbName] && typeof global[cbName] === 'function') {
+                        if (global[cbName] && typeof global[cbName] === 'function') {
                             global[cbName] = null;
-                            delete global[cbName];
                         }
-                    }
+                    };
 
                     tag.onload = handleResult('loaded');
-                    tag.onerror = handleResult('error');
+                    tag.onerror = handleResult('error')
                     tag.onreadystatechange = () => {
                         handleResult(tag.readyState)
-                    }
+                    };
 
-                    //Pick off callback
-                    if(src.match(/callback=CALLBACK_NAME/)) {
-                        const regex = /(callback=)[^\&]+/;
-                        src = src.replace(regex, `$1${cbName}`);
+                    // Pick off callback, if there is one
+                    if (src.match(/callback=CALLBACK_NAME/)) {
+                        src = src.replace(/(callback=)[^\&]+/, `$1${cbName}`)
                         cb = window[cbName] = tag.onload;
-                    }
-                    else {
-                        tag.addEventListener('load', tag.onload);
+                    } else {
+                        tag.addEventListener('load', tag.onload)
                     }
                     tag.addEventListener('error', tag.onerror);
 
@@ -99,20 +79,17 @@ export const ScriptCache = (function(global) {
                     error: false,
                     promise: promise,
                     tag
-                }
+                };
                 scriptMap.set(key, initialState);
             }
             return scriptMap.get(key);
-        }
+        };
 
         Object.keys(scripts).forEach(function(key) {
             const script = scripts[key];
-
-            const tag = window._scriptMap.has(key) ? window._scriptMap.get(key).tag : Cache._scriptTag(key, script);
-
             Cache[key] = {
-                tag: tag,
-                onLoad: Cache._onLoad(key),
+                tag:    Cache._scriptTag(key, script),
+                onLoad: Cache._onLoad(key)
             }
         });
 
